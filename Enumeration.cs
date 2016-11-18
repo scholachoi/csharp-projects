c5a022cf6b727bac86e1e8ad7e56cb90d4fb728c
Schola Choi <scholachoi@gmail.com>
2016-11-17 16:15:11 -0800

    Add files via upload

---
+++ Enumeration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Enumeration
+{
+    enum Customer { Adult, Child, Senior, Student, Veteran }
+   
+    static class Movie
+    {
+        static decimal CalculatePrice(Customer customer)
+        {
+            switch (customer)
+            {
+                case Customer.Adult:
+                    return 12.00m;
+                    
+                case Customer.Child:
+                    return 9.00m;
+                   
+                case Customer.Senior:
+                    return 9.50m;
+                   
+                case Customer.Student:
+                    return 11.00m;
+                   
+                case Customer.Veteran:
+                    return 10.00m;
+                   
+                default:
+                    return 12.00m;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Movie.CalculatePrice(Customer.Child));
+
+        }
+    }
+}
