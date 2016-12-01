a5527bc5c849658c3d0a3414992fc4c54dc1ef5f
Schola Choi <scholachoi@gmail.com>
2016-12-01 13:44:19 -0800

    Add files via upload

---
+++ FileCopy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace FileCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileCheck();
+        }
+
+        static void FileCheck()
+        {
+            var source = new DirectoryInfo(@"C:\Users\Student\Desktop\Source");
+            var destination = @"C:\Users\Student\Desktop\Destination";
+
+            Directory.CreateDirectory(destination);
+
+            try
+            {
+                foreach (var file in source.GetFiles())
+                {
+                    if (DateTime.Now <= file.LastWriteTime.AddHours(24))
+                    {
+                        file.CopyTo(Path.Combine(destination, file.Name));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
